dc892756e0d8c1f291a51f1b60bc3ba62f09bddd
rohinimathur <ladyroho@gmail.com>
2021-07-16 17:32:00 +0100

    update codelists

--- analysis/codelists.py
+++ analysis/codelists.py
@@ -1,5 +1,9 @@
+from cohortextractor import (
+    codelist,
+    codelist_from_csv,
+)
 ethnicity_codes = codelist_from_csv(
-    "codelists/opensafely-nice-managing-the-long-term-effects-of-covid-19.csv",
+    "codelists/ethnicity-uk-categories/3d6551e9.csv",
     system="snomed",
     column="code",
 )
