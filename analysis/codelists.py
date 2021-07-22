c4b00b5fcb4c1ac83d8742acf78d1b16e4b5f17d
rohinimathur <ladyroho@gmail.com>
2021-07-22 11:34:55 +0100

    update ethnicity codelist to latest version

--- analysis/codelists.py
+++ analysis/codelists.py
@@ -3,7 +3,7 @@ from cohortextractor import (
     codelist_from_csv,
 )
 ethnicity_codes = codelist_from_csv(
-    "codelists/opensafely-ethnicity-uk-categories.csv",
+    "codelists/opensafely-ethnicity-uk-categories/516dea7b.csv",
     system="snomed",
     column="code",
 )
