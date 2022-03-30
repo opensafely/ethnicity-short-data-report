40223bf97362e44f782b47ce8e954d087abf0426
andrewscolm <73839417+andrewscolm@users.noreply.github.com>
2022-03-30 12:03:16 +0100

    add Rmd

--- analysis/study_definition.py
+++ analysis/study_definition.py
@@ -1,9 +1,11 @@
 from cohortextractor import StudyDefinition, patients, codelist, codelist_from_csv, combine_codelists  # NOQA
 from create_variables import demographic_variables, clinical_variables
 from codelists import *
+from config import *
+
 
 study = StudyDefinition(
-    index_date="2015-01-01",
+    index_date=index_date,
     default_expectations={
         "date": {"earliest": "index_date", "latest": "last_day_of_month(index_date)"},
         "rate": "uniform",
@@ -11,21 +13,8 @@ study = StudyDefinition(
     },
     population=patients.satisfying(
         """
-        registered AND
         (sex = "M" OR sex = "F")
         """,
-        # Looking at registered patients yearly
-        registered=patients.registered_with_one_practice_between(
-            "index_date", "last_day_of_month(index_date)",
-            return_expectations={"incidence": 0.9},
-        )
-    ),
-
-    # Deregistration date (to censor these patients in longitudinal analyses)
-    dereg_date=patients.date_deregistered_from_all_supported_practices(
-        between=["2015-01-01", "2021-12-31"],
-        date_format="YYYY-MM-DD",
-        return_expectations={"date": {"earliest": "index_date"}},
     ),
 
     # Death date (to censor these patients in longitudinal analyses)
