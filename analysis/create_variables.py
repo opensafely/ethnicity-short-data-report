9c922dc3e5425f9abaa47a9e6793a004e943d4da
andrewscolm <73839417+andrewscolm@users.noreply.github.com>
2022-06-13 17:00:31 +0100

    fix imd definition

--- analysis/create_variables.py
+++ analysis/create_variables.py
@@ -350,11 +350,11 @@ demographic_variables = dict(
     imd=patients.categorised_as(
         {
             "0": "DEFAULT",
-            "1": """index_of_multiple_deprivation >=0 AND index_of_multiple_deprivation < 32844*1/5""",
-            "2": """index_of_multiple_deprivation >= 32844*1/5 AND index_of_multiple_deprivation < 32844*2/5""",
-            "3": """index_of_multiple_deprivation >= 32844*2/5 AND index_of_multiple_deprivation < 32844*3/5""",
-            "4": """index_of_multiple_deprivation >= 32844*3/5 AND index_of_multiple_deprivation < 32844*4/5""",
-            "5": """index_of_multiple_deprivation >= 32844*4/5 """,
+            "1": """index_of_multiple_deprivation >=0 AND index_of_multiple_deprivation < 32800*1/5""",
+            "2": """index_of_multiple_deprivation >= 32800*1/5 AND index_of_multiple_deprivation < 32800*2/5""",
+            "3": """index_of_multiple_deprivation >= 32800*2/5 AND index_of_multiple_deprivation < 32800*3/5""",
+            "4": """index_of_multiple_deprivation >= 32800*3/5 AND index_of_multiple_deprivation < 32800*4/5""",
+            "5": """index_of_multiple_deprivation >= 32800*4/5 AND index_of_multiple_deprivation <= 32800 """,
         },
         index_of_multiple_deprivation=patients.address_as_of(
             "index_date",
