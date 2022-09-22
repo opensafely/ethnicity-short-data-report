4f948e93699020460f0cabbf78481b6bb2b37357
andrewscolm <73839417+andrewscolm@users.noreply.github.com>
2022-09-22 15:59:00 +0100

    add range of recorded ethnicities

--- analysis/iqr.R
+++ analysis/iqr.R
@@ -14,20 +14,41 @@ input<-arrow::read_feather(here::here("output","data","input.feather")) %>%
          ethnicity_5=as.numeric(as.character(ethnicity_5)),
          ethnicity_primis_5=as.numeric(as.character(ethnicity_primis_5)))
 
-summary_all <- input %>% 
-       summarise_at(vars(ethnicity_5,ethnicity_new_5,ethnicity_primis_5), na.rm = TRUE,
-                                       list(min=min, Q1=~quantile(., probs = 0.25, na.rm = TRUE),
-                                                                    median=median, Q3=~quantile(., probs = 0.75, na.rm = TRUE),
-                                                                    max=max))
+summary_full <- input %>% 
+  summarise_at(vars(ethnicity_5,ethnicity_new_5,ethnicity_primis_5), na.rm = TRUE,
+               list(min=min, Q1=~quantile(., probs = 0.25, na.rm = TRUE),
+                    median=median, Q3=~quantile(., probs = 0.75, na.rm = TRUE),
+                    max=max))
 
-summary_all<-summary_all  %>% pivot_longer(
-       everything(),
-       names_to = c( "codelist","measure"),
-       names_pattern = "(.*)_(.*)",
-       values_to = "value"
-   )  %>% pivot_wider(names_from = measure, values_from = value) %>%
+summary_full<-summary_full  %>% pivot_longer(
+  everything(),
+  names_to = c( "codelist","measure"),
+  names_pattern = "(.*)_(.*)",
+  values_to = "value"
+)  %>% pivot_wider(names_from = measure, values_from = value) %>%
   rowwise() %>%
   mutate(n_max = nrow(input[which(input[,codelist]==max),]),
          n_min = nrow(input[which(input[,codelist]==min),]))
 
-write_csv(summary_all,here::here("output", "simplified_output","5_group","tables","range.csv"))
+write_csv(summary_full,here::here("output", "simplified_output","5_group","tables","range_fullset.csv"))
+
+input_reg <-input %>%
+  filter(registered)
+
+summary_reg <- input_reg %>%
+  summarise_at(vars(ethnicity_5,ethnicity_new_5,ethnicity_primis_5), na.rm = TRUE,
+               list(min=min, Q1=~quantile(., probs = 0.25, na.rm = TRUE),
+                    median=median, Q3=~quantile(., probs = 0.75, na.rm = TRUE),
+                    max=max))
+
+  summary_reg<-summary_reg  %>% pivot_longer(
+  everything(),
+  names_to = c( "codelist","measure"),
+  names_pattern = "(.*)_(.*)",
+  values_to = "value"
+)  %>% pivot_wider(names_from = measure, values_from = value) %>%
+  rowwise() %>%
+  mutate(n_max = nrow(input_reg[which(input_reg[,codelist]==max),]),
+         n_min = nrow(input_reg[which(input_reg[,codelist]==min),]))
+
+write_csv(summary_reg,here::here("output", "simplified_output","5_group","tables","range_registered.csv"))
